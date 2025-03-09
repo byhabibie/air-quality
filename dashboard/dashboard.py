be84851afeb3a2a8d37686d811ec55b1a000e95b
Moch. Baharudin Yusuf Habibie <122619731+byhabibie@users.noreply.github.com>
2025-03-09 12:17:32 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,7 +6,7 @@ import streamlit as st
 sns.set(style='white')
 
 # Load Data
-df = pd.read_csv("https://raw.githubusercontent.com/sfhabibie/air-quality/main/dashboard/main_data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/byhabibie/air-quality/main/dashboard/main_data.csv")
 df['datetime'] = pd.to_datetime(df[['year', 'month', 'day', 'hour']])
 df['year_month'] = df['datetime'].dt.to_period('M')
 
